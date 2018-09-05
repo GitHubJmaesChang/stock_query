be7774b90bc48d141a38769b19a575fbb8071903
GitHubJmaesChang <35448917+GitHubJmaesChang@users.noreply.github.com>
2018-09-05 18:03:34 +0800

    Add files via upload
    -update the table merge method for Q1 ~Q4

--- merge_table_stock.py
+++ merge_table_stock.py
@@ -5,12 +5,14 @@
 import os
 import requests
 import pandas as pd
-import numpy as np
+import numpy
 import csv, codecs, urllib, datetime, time, pdb
+import copy
 
 File_Location = 'D://Stock/finacial/'
 Table_name    = "basic_report"
 
+NO_UPDATE = 0
 Q1_TABLE = 1
 Q2_TABLE = 2
 Q3_TABLE = 3
@@ -28,13 +30,34 @@ def check_report(FileName, FilePath):
     return open_status
 
 
-
 def insert_row(idx, df, df_insert):
     dfA = df.iloc[:idx, ]
     dfB = df.iloc[idx:, ]
     df = dfA.append(df_insert).append(dfB).reset_index(drop = True)
-    df = df.drop([df.columns[0]], axis =1)
     return df
+
+# in order to reduce the trace resource, we mask the max search length = 10
+def get_data_from_table_list(stock_numbs_table, Qx_target_table, column_idx):
+    current_search_pos = 0
+    searched = 0
+    row=[]
+    for idx in range(0, len(stock_numbs_table)):
+        for chk in range(current_search_pos, current_search_pos + 10 ):
+            if(stock_numbs_table[idx] == Qx_target_table.loc[current_search_pos][1]):
+                row.append(Qx_target_table.loc[current_search_pos][column_idx])
+                current_search_pos+=1
+                searched =1
+                break
+            else:
+                searched =0
+        if(searched ==0):
+            row.append(0)
+    return row
+
+def update_list(cmp1, cmp2):
+    for idx in range(0, len(cmp1)):
+        if(cmp1[idx] == 0 and (cmp2[idx])):
+            cmp1[idx] = cmp2[idx]
 
 def merge_table(year, FilePath):
     table_numbs =0
@@ -84,32 +107,105 @@ def merge_table(year, FilePath):
 
     # be used to check which table lost items
     lost_data_target_is_numb =0
+    update_table = 0
+
+    stock_numbs=[]
+  #add Q1 company id at first
+    for idx in range(0, Q1_table.shape[0]):
+        stock_numbs.append(Q1_table.loc[idx][1])
+
+  # Q2 table vs Q1 list
+    print "check Q2 table"
+    numbs_idx =0
+    if(table_numbs==2):
+        for idx in range(0, Q2_table.shape[0]):
+            if(stock_numbs[numbs_idx] != Q2_table.loc[idx][1]):
+                update_table = Q1_TABLE
+                for ch in range(numbs_idx, len(stock_numbs)):
+                    if(Q2_table.loc[idx][1] == stock_numbs[ch]):
+                        update_table = NO_UPDATE
+                        #update the index to correct position
+                        numbs_idx = ch
+                        break
+                if(update_table == Q1_TABLE):
+                    stock_numbs.insert(numbs_idx, Q2_table.loc[idx][1])
+
+    print "check Q2 table done"
+   # Q3 table vs Q1 list
+    numbs_idx =0
+    if(table_numbs ==3):
+        for idx in range(0, Q3_table.shape[0]):
+            if(stock_numbs[numbs_idx] != Q3_table.loc[idx][1]):
+                update_table = Q3_TABLE
+                for ch in range(numbs_idx, len(stock_numbs)):
+                    if(Q3_table.loc[idx][1] == stock_numbs[ch]):
+                        update_table = NO_UPDATE
+                        #update the index to correct position
+                        numbs_idx = ch
+                        break
+                if(update_table == Q3_TABLE):
+                    stock_numbs.insert(numbs_idx, Q3_table.loc[idx][1])
+
+   # Q4 table vs Q1 list
+    numbs_idx =0
+    if(table_numbs==4):
+        for idx in range(0, Q4_table.shape[0]):
+            if(stock_numbs[numbs_idx] != Q4_table.loc[idx][1]):
+                update_table = Q4_TABLE
+                for ch in range(numbs_idx, len(stock_numbs)):
+                    if(Q4_table.loc[idx][1] == stock_numbs[ch]):
+                        update_table = NO_UPDATE
+                        #update the index to correct position
+                        numbs_idx = ch
+                        break
+                if(update_table == Q4_TABLE):
+                    stock_numbs.insert(numbs_idx, Q4_table.loc[idx][1])
+
+    stock_numbs = sorted(stock_numbs)
+    print "add company name"
+    company_name =[]
+    company_name_temp =[]
+    company_name = list(get_data_from_table_list(stock_numbs, Q1_table, Q1_table.columns[2]))
+    if(table_numbs >= 2):
+        company_name_temp = list(get_data_from_table_list(stock_numbs, Q2_table, Q2_table.columns[2]))
+        update_list(company_name, company_name_temp)
+    if(table_numbs >= 3):
+        company_name_temp = list(get_data_from_table_list(stock_numbs, Q3_table, Q3_table.columns[2]))
+        update_list(company_name, company_name_temp)
+    if(table_numbs >= 4):
+        company_name_temp = list(get_data_from_table_list(stock_numbs, Q4_table, Q4_table.columns[2]))
+        update_list(company_name, company_name_temp)
+
+    sotck_id_form = pd.DataFrame({ Q1_table.columns[1] : stock_numbs})
+    name_form = pd.DataFrame({ Q1_table.columns[2] : company_name})
+    df = pd.concat( [ sotck_id_form[Q1_table.columns[1] ],
+                      name_form[Q1_table.columns[2]]], axis =1)
+
+    df.to_csv( FilePath + "test" + str(1) + ".csv", encoding = "utf-8") 
+    return 
     
     for idx in range(0, max_length[1]):
         if(table_numbs<=2):
-            if(Q1_table[u'公司代號'.encode('utf-8')][idx] != Q2_table[u'公司代號'.encode('utf-8')][idx] ) :
-                print Q1_table[u'公司代號'.encode('utf-8')][idx]
-                print Q2_table[u'公司代號'.encode('utf-8')][idx]
+            if(Q1_table.loc[idx][1] != Q2_table.loc[idx][1] ) :
                 #Step 1 : check who's data has lost
                 for c in range(idx ,Q2_table.shape[0]):
-                    if(Q1_table[u'公司代號'.encode('utf-8')][idx] == Q2_table[u'公司代號'.encode('utf-8')][c]) :
+                    if(Q1_table.loc[idx][1] == Q2_table.loc[c][1]) :
                        lost_data_target_is_numb = Q1_TABLE
                        break
                     else:
                         lost_data_target_is_numb = Q2_TABLE
                 if(lost_data_target_is_numb ==Q1_TABLE):
-                    Q1_table = insert_row(idx, Q1_table, Q2_table.loc[idx][2:])
-                    print Q1_table
-                    print Q2_table
+                    Q1_table = insert_row(idx, Q1_table, Q2_table.loc[idx])
                     print "Q1_table drop"
                 if(lost_data_target_is_numb ==Q2_TABLE):
-                    Q2_table = insert_row(idx, Q2_table, Q1_table.loc[idx][2:])
+                    Q2_table = insert_row(idx, Q2_table, Q1_table.loc[idx])
                     print "Q2_table drop"
-                print ("lost item at Q")+str(lost_data_target_is_numb) + ", position is " + str(idx)
-                break;
+                if(Q1_table.columns[0] == "Unnamed: 0"):
+                    Q1_table = Q1_table.drop([Q1_table.columns[0]], axis =1)
 
-    Q1_table.to_csv(FilePath + FileName + str(1) + "1.csv", encoding = "utf-8")
-    Q2_table.to_csv(FilePath + FileName + str(2) + "2.csv", encoding = "utf-8")
+                if(Q2_table.columns[0] == "Unnamed: 0"):
+                    Q2_table = Q2_table.drop([Q2_table.columns[0]], axis =1)
+                print ("lost item at Q")+str(lost_data_target_is_numb) + ", position is " + str(idx)
      
 if __name__ == '__main__':
     merge_table(2018, File_Location)
