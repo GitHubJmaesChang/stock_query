330f5539ee9092bef86fd8a17df5dd8db2f09a1d
ChiaMing Chang <35448917+GitHubJmaesChang@users.noreply.github.com>
2018-10-04 18:06:50 +0800

    Add files via upload
    - integrate query function code 
    -add db function

--- stock.py
+++ stock.py
@@ -6,11 +6,45 @@ import os
 import pandas as pd
 import time
 
-from query import query_company_data
-from query import query_stock_date
+from query import query_company_id 
+from query import query_financialStatement
+from query import query_institutional_investors_info
+from query import query_margin_and_short_trade
+from query import query_month_income
+from query import query_stock_dailydata
+from query import query_stock_info_by_ID
+
+from db import init_db_utf8
+from db import add_db_record
+
 
 comp_id   = 'comp_id.csv'
-File_Path = 'D://Stock/stock_trade//'
+File_Path = 'D://Stock/stock_trade/'
+
+def intial_db():
+    
+def data_query_company_id():
+    query_company_id.query_public_trade_company_ID()
+    
+def data_query_financialStatement():
+    query_financialStatement.update_company_state(2017, 4, File_Path)
+    
+def data_query_institutional_investors_info():
+    query_institutional_investors_info.daily_institutional_info(File_Path , "20181001")
+    
+def data_query_margin_and_short_trade():
+    query_margin_and_short_trade.daily_information(File_Path , "20181002")
+    
+def data_query_month_income():
+    query_month_income.monthly_report(File_Path, 2018, 8)
+    
+def data_query_stock_dailydata():
+    query_stock_dailydata.daily_information(File_Path , "20181001")
+    
+def data_query_stock_info_by_ID():
+    query_stock_info_by_ID.stock_query('1101', File_Path)
+    
+
 
 if __name__ == '__main__':
     path = os.path.dirname(os.path.abspath(__file__))
