ed1cf9334874bc90278d0f5e4ec5285252fe789c
JamesChang <6826yf@gmail.com>
2018-10-11 08:12:46 +0800

    -upload stock.py for query and database link

--- stock.py
+++ stock.py
@@ -1,12 +1,12 @@
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import pandas as pd
 import time
 
-from query import query_company_id 
+from query import query_company_id
 from query import query_financialStatement
 from query import query_institutional_investors_info
 from query import query_margin_and_short_trade
@@ -17,13 +17,23 @@ from query import query_stock_info_by_ID
 from db import init_db_utf8
 from db import add_db_record
 
+TRUE  = 1
+FALSE = 0
 
 comp_id   = 'comp_id.csv'
-File_Path = 'D://Stock/stock_trade/'
+File_Path = "D:/Stock/finacial/"
+
+
+def check_file_exist(filename):
+    if(os.path.isfile(filename)):
+        return TRUE
+    else:
+        return FALSE
+
 
 # crate database : 
 def intial_db():
-    InitDB()
+    init_db_utf8.InitDB()
 
 # update company ID
 def data_query_company_id():
@@ -51,8 +61,13 @@ def data_query_stock_dailydata(path, date):
 def data_query_stock_info_by_ID(path, Stock_id):
     query_stock_info_by_ID.stock_query(Stock_id, path)
 
-def query_daily_date(file_path, date):
+def insert_date_into_database(file_name):
     intial_db()
+    if(FALSE == check_file_exist(file_name)):
+        return FALSE
+
+    pd_form = pd.read_csv(file_name)
+    return pd_form
 
     
 
@@ -61,8 +76,11 @@ if __name__ == '__main__':
     #query_company_data.update_company_state(2018,1, path + "//fincial_report//")
     #company_id = pd.read_csv(path + "/fincial_report/" + comp_id)
     
-    for idx in range(0, company_id.shape[0]):
-        query_stock_date.stock_query( str(company_id.iloc[idx][1]), path + "//stock_report//")
-        time.sleep(5)
+    #for idx in range(0, company_id.shape[0]):
+    #    query_stock_date.stock_query( str(company_id.iloc[idx][1]), path + "//stock_report//")
+    #    time.sleep(5)
+
+    target_file = File_Path + "basic_report107_s1.csv"
+    print "save data to DB = " + (insert_date_into_database(target_file))
     
     print "process done"
